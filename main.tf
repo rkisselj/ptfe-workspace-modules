6a09da2ddf6ffef918b468763983999b2746cf88
rkisselj <48292150+rkisselj@users.noreply.github.com>
2021-01-28 13:08:10 -0800

    Update source arguments to your organization
    Update the source arguments to your organization by replacing "YOUR_ORG_NAME" with your TFE organization name.

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ variable "subnet_address_prefixes" {
 }
 
 module "networking" {
-  source  = "app.terraform.io/YOUR_ORG_NAME/networking/azurerm"
+  source  = "app.terraform.io/training-rtk/networking/azurerm"
   version = "0.12.0"
 
   name                    = var.name
